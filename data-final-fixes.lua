a55cbdf98c1c1974fb144d00b2d634b7152866ea
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-05-08 20:39:16 +0300

    feat: fixes Nullius <-> Renai chest issue

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,3 +5,11 @@ if entity then
         entity.energy_source.emissions_per_minute["electromagnetic_waves"] = nil
     end
 end
+
+if mods["nullius"] then
+    data.raw["recipe"]["wooden-chest"].localised_name = {"entity-name.nullius-small-chest-1"}
+    data.raw["recipe"]["wooden-chest"].ingredients = {
+      {type = "item", name = "nullius-iron-sheet", amount = 2},
+      {type = "item", name = "nullius-iron-rod", amount = 1}
+    }
+end
